25ad6a605a7f916d2bef568c0d94dc41d9ed5020
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-08-12 16:30:50 -0300

    setup routes and validation

--- src/billets/billets.controller.ts
+++ src/billets/billets.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateBilletDto } from './dtos/create-billet.dto';
 
 @Controller('billets')
-export class BilletsController {}
+export class BilletsController {
+  @Get()
+  listBillets() {}
+
+  @Post()
+  createBillet(@Body() body: CreateBilletDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getBillet(@Param('id') id: string) {
+    console.log(id);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { BilletsModule } from './billets/billets.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(BilletsModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
+
 bootstrap();
