a60118682cb59911e3f3d8a85d5cb68fb6e7b1f6
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-08-12 18:00:20 -0300

    feat: post route to pay billets

--- src/billets/billets.controller.ts
+++ src/billets/billets.controller.ts
@@ -1,24 +1,25 @@
 import axios from 'axios';
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Response } from '@nestjs/common';
 import { CreateBilletDto } from './dtos/create-billet.dto';
 import { BilletsService } from './billets.service';
 
 @Controller('billets')
 export class BilletsController {
-  constructor(billetsService: BilletsService) {}
-
   private billetsService: BilletsService = new BilletsService();
 
   @Get()
   listBillets() {}
 
   @Post()
-  createBillet(@Body() body: CreateBilletDto) {
-    console.log(this.billetsService.sendBillet(body));
-    
-    this.billetsService.sendBillet(body);
+  createBillet(@Body() body: CreateBilletDto, @Response() res) {
+    const url = 'https://run.mocky.io/v3/0bca48f0-16db-4726-96a8-d4206306f698'
+    return axios.post(url, body).then(function (response) {
+      res.send(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
   }
-
 
   // @Get('/:id')
   // getBillet(@Param('id') id: string) {
