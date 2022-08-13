a097474971049babdf490c61cec5a423a03400d0
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-08-13 16:41:01 -0300

    refactor interceptor to make it generic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Delete, Get, NotFoundException, Param, Post, UseInterceptors } from '@nestjs/common';
 import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -14,7 +15,7 @@ export class UsersController {
     return {id: user.id};
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string){
     const user = await this.usersService.findById(parseInt(id));
@@ -28,7 +29,7 @@ export class UsersController {
     return user;
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Delete('/:id')
   async removeUser(@Param('id') id: string){
     const user = await this.usersService.findById(parseInt(id));
