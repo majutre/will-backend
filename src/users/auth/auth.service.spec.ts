3617e1b0886f8ebaaa3ebffeae96568454265939
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-08-14 12:23:09 -0300

    minor test refactor

--- src/users/auth/auth.service.spec.ts
+++ src/users/auth/auth.service.spec.ts
@@ -1,13 +1,15 @@
 import { Test } from '@nestjs/testing';
+
 import { AuthService } from './auth.service';
 import { UsersService } from '../users.service';
+import { User } from '../user.entity';
 
 describe('AuthService', () => {
   it('should create an instance of AuthService', async () => {
-    const usersService = {
+    const usersService: Partial<UsersService> = {
       findByEmail: () => Promise.resolve([]),
       create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password }),
+        Promise.resolve({ id: 1, email, password } as User),
     };
 
     const module = await Test.createTestingModule({
