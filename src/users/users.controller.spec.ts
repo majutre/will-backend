129ca67d1b1c114866ffc77e4fff7158d4d3ba39
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-08-14 13:41:50 -0300

    specs: users controller (wip)

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,47 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { AuthService } from './auth/auth.service';
+
+import { User } from './user.entity';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let usersService: Partial<UsersService>;
+  let authService: Partial<AuthService>;
 
-  // beforeEach(async () => {
-  //   const module: TestingModule = await Test.createTestingModule({
-  //     controllers: [UsersController],
-  //   }).compile();
+  beforeEach(async () => {
+    usersService = {
+      findById: (id: number) => {
+        return Promise.resolve({id, email: 'asdf@asdf', password: '1a2b3c'} as User);
+      },
+      remove: (id: number) => {
+        return Promise.resolve({id, email: 'asdf@asdf', password: '1a2b3c'} as User);
+      },
+    }
+    authService = {
+      // registration: () => {},
+      // login: () => {},
+    }
 
-  //   controller = module.get<UsersController>(UsersController);
-  // });
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: usersService
+        },
+        {
+          provide: AuthService,
+          useValue: authService
+        },
+      ]
+    }).compile();
 
-  xit('should be defined', () => {
+    controller = module.get<UsersController>(UsersController);
+  });
+
+  it('should be defined', () => {
     expect(controller).toBeDefined();
   });
 });
