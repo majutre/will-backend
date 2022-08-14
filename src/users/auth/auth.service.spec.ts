9df67e292b100ae63f8098792b57873296093fe2
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-08-14 16:57:56 -0300

    refactor specs

--- src/users/auth/auth.service.spec.ts
+++ src/users/auth/auth.service.spec.ts
@@ -8,6 +8,8 @@ import { User } from '../user.entity';
 describe('AuthService', () => {
   let service: AuthService;
   let usersService: Partial<UsersService>;
+  const emailSample = 'asdf@asdf'
+  const passwordSample = '123456'
 
   beforeEach(async () => {
     const users: User[] = [];
@@ -45,36 +47,36 @@ describe('AuthService', () => {
   });
 
   it('should create a new user with salted and hashed password', async () => {
-    const user = await service.registration('asdf@asdf', '123456');
+    const user = await service.registration(emailSample, passwordSample);
 
-    expect(user.password).not.toEqual('123456');
+    expect(user.password).not.toEqual(passwordSample);
     const [salt, hash] = user.password.split('.');
     expect(salt).toBeDefined();
     expect(hash).toBeDefined();
   });
 
   it('should return a user if correct password is provided', async () => {
-    await service.registration('asdf@asdf', '123456');
-    const user = await service.login('asdf@asdf', '123456');
+    await service.registration(emailSample, passwordSample);
+    const user = await service.login(emailSample, passwordSample);
     expect(user).toBeDefined();
   });
 
   it('should throw an error if user signs up with an e-mail in use', async () => {
-    await service.registration('asdf@asdf', '123456');
-    await expect(service.registration('asdf@asdf', '123456')).rejects.toEqual(
+    await service.registration(emailSample, passwordSample);
+    await expect(service.registration(emailSample, passwordSample)).rejects.toEqual(
       new BadRequestException('Este e-mail já está em uso'),
     );
   });
 
   it('should throw an error if user tries to login with an unused email', async () => {
-    await expect(service.login('asdf@asdf', '123456')).rejects.toEqual(
+    await expect(service.login(emailSample, passwordSample)).rejects.toEqual(
       new BadRequestException('E-mail ou senha inválidos'),
     );
   });
 
   it('should throw an error if invalid password is provided', async () => {
-    await service.registration('asdf@asdf', '1a2b3c');
-    await expect(service.login('asdf@asdf', '123456')).rejects.toEqual(
+    await service.registration(emailSample, '1a2b3c');
+    await expect(service.login(emailSample, passwordSample)).rejects.toEqual(
       new BadRequestException('E-mail ou senha inválidos'),
     );
   });
--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from '../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const emailSample = 'e2e@teste.com';
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -16,14 +17,29 @@ describe('AppController (e2e)', () => {
   });
 
   it('should handle a registration request', () => {
-    const reqEmail = 'e2e@teste.com'
     return request(app.getHttpServer())
       .post('/users/sign_up')
-      .send({ email: reqEmail, password: '1a2b3c' })
+      .send({ email: emailSample, password: '1a2b3c' })
       .expect(201)
       .then((res) => {
         const id = res.body;
         expect(id).toBeDefined();
       });
+  });
+
+  it('get currently logged in user, if any', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/users/sign_up')
+      .send({ email: emailSample, password: '1a2b3c' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/users/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(emailSample);
   });
 });
