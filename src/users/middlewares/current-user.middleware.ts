53cf2a3bd27580425fbc680bd86c0290b97b6746
Maria Julia Trevisan <maria.trevisan@nexoos.com.br>
2022-08-14 21:40:12 -0300

    fix type errors

--- src/users/middlewares/current-user.middleware.ts
+++ src/users/middlewares/current-user.middleware.ts
@@ -1,9 +1,23 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
 import { NextFunction, Request, Response } from "express";
 import * as session from 'express-session';
+import { User } from "../user.entity";
 
 import { UsersService } from "../users.service";
 
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
+
+declare module 'express-session' {
+  export interface SessionData {
+    userId: number;
+  }
+}
 @Injectable()
 export class CurrentUserMiddleware implements NestMiddleware {
   constructor(
@@ -11,15 +25,10 @@ export class CurrentUserMiddleware implements NestMiddleware {
   ) {}
 
   async use(req: Request, res: Response, next: NextFunction) {    
-      //@ts-ignore
-    
     const userId = req.session.userId;
 
     if (userId) {
-      //@ts-ignore
       const user = await this.usersService.findById(userId);
-      
-      //@ts-ignore
       req.currentUser = user;
     }
 
